5725e8e303f0186aab97dc8abf4c6b818c4fa611
yaserahmedn <41563530+yaserahmedn@users.noreply.github.com>
2018-09-29 22:16:33 +0530

    Add files via upload

---
+++ lcd_counter.ino
@@ -0,0 +1,73 @@
+//Visitors Counter and Automatic Room Light
+//Viral Science  www.youtube.com/c/viralscience
+
+#include<LiquidCrystal_I2C.h>
+#include<Wire.h>
+LiquidCrystal_I2C lcd(0x3f,16,2);
+
+#define in D1
+#define out D2
+#define relay D3  //LED
+
+int count=0;
+
+void IN()
+{
+    count++;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+}
+
+void OUT()
+{
+  count--;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+}
+
+void setup()
+{
+  lcd.init();
+  Wire.begin(D4,D5);
+  lcd.begin(16,2);
+  lcd.print("Visitor Counter");
+  delay(2000);
+  pinMode(in, INPUT);
+  pinMode(out, INPUT);
+  pinMode(relay, OUTPUT);
+  lcd.backlight();
+  lcd.clear();
+  lcd.print("Person In Room:");
+  lcd.setCursor(0,1);
+  lcd.print(count);
+}
+
+void loop()
+{  
+  
+  while(digitalRead(in))
+  IN();
+  while(digitalRead(out))
+  OUT();
+  
+  if(count<=0)
+  {
+    lcd.clear();
+    digitalWrite(relay, LOW);
+    lcd.clear();
+    lcd.print("Nobody In Room");
+    lcd.setCursor(0,1);
+    lcd.print("Light Is Off");
+    delay(200);
+  }
+  
+  else
+    digitalWrite(relay, HIGH);
+  
+}
